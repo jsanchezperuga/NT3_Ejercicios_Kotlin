4b3ca724cbf13508b8c32634977afa44f51a775a
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-04-19 19:29:57 -0300

    Challenge pass data between fragments done

---
+++ recyclerview/src/main/java/com/nt3/recyclerview/Event.kt
@@ -0,0 +1,4 @@
+package com.nt3.recyclerview
+
+data class Event (val eventName:String, val eventDate:String, val eventTime:String, val eventImage:String){
+}
---
+++ recyclerview/src/main/java/com/nt3/recyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nt3.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
